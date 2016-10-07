ba313c10c6951f356147c16aff1469b8d4816172
Jan Burkhardt <jan.burkhardt@just.social>
2016-10-07 14:06:05 +0200

    a simple render test
    Change-Id: I862a3bfd31055b14c6802473072898b0899e37ff

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = require('./lib/magic-scroll.jsx');
+module.exports = require('./lib/magic-scroll.js');
---
+++ src/test/js/magic-scroll.spec.js
@@ -0,0 +1,34 @@
+/*eslint-env node, mocha */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactTestUtils, { Simulate } from 'react-addons-test-utils';
+import expect from 'expect';
+import expectElement from 'expect-element';
+expect.extend(expectElement);
+
+import { MagicScroll } from 'just-magic-scroll/magic-scroll.jsx';
+
+describe('MagicScroll component test', () => {
+
+    const defaultProps = {
+
+        itemHeight: 50,
+        itemCount: 20,
+        rowFunction: (index) => { return (<div key={index}>index</div>) },
+        fetch: () => { }
+    }
+
+    function renderMagicScroll(props = {}) {
+        const validProps = Object.assign({}, defaultProps, props);
+        const result = ReactTestUtils.renderIntoDocument(
+            <MagicScroll {...validProps}/>
+        );
+        return ReactTestUtils.findRenderedComponentWithType(result, MagicScroll);
+    }
+
+    it('should render', () => {
+        expect(renderMagicScroll()).toBeA(MagicScroll);
+    });
+
+});
